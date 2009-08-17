384ba4ce90829c47765cabfc731098ed8fe25588
Luca Spiller <luca@stackednotion.com>
2009-08-17 10:21:13 +0100

    Add initial files Test: Ysfo: It Must Enable The Path To Be Set

---
+++ Src/Core/Ysfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ysfo.Core
+{
+    public class Ysfo : IDisposable
+    {
+        public String Path { get; set; }
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+        }
+
+        #endregion
+    }
+}
---
+++ Tests/Core/YsfoFixture.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Ysfo.Tests.Core
+{
+    [TestFixture]
+    class YsfoFixture
+    {
+        protected const string ysPath = @"C:\dev\ysflight\20090502";
+
+        [Test]
+        public void ItMustEnableThePathToBeSet()
+        {
+            using (var ysfo = new Ysfo.Core.Ysfo())
+            {
+                ysfo.Path = ysPath;
+            }
+        }
+    }
+}
