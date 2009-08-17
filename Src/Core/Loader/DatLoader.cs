c826c333e4c2e9e30a6686074ee9e00cc9b77592
Luca Spiller <luca@stackednotion.com>
2009-08-17 21:56:07 +0100

    Add DatLoader

---
+++ Src/Core/Loader/DatLoader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ysfo.Core.Loader
+{
+    public static class DatLoader
+    {
+        public static String GetDatFileFromLstEntry(String lstEntry)
+        {
+            // split lst
+            String[] split = lstEntry.Split(' ');
+
+            // loop through parts
+            foreach (String part in split)
+            {
+                // look for match
+                if (part.ToLower().Contains(".dat"))
+                {
+                    return part;
+                }
+            }
+
+            // no match
+            throw new ArgumentException("No dat file found in lstEntry.");
+        }
+    }
+}
---
+++ Tests/Core/Loader/DatLoaderFixture.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Ysfo.Tests.Core.Loader
+{
+    [TestFixture]
+    class DatLoaderFixture
+    {
+        [Test]
+        public void Extractor_ItShouldReturnDatFileFromLstEntry()
+        {
+            String lstEntry = "aircraft/aircraft.dat aircraft/aircraft.dnm";
+
+            String result = Ysfo.Core.Loader.DatLoader.GetDatFileFromLstEntry(lstEntry);
+
+            Assert.AreEqual("aircraft/aircraft.dat", result);
+        }
+
+        [Test, ExpectedException(typeof(ArgumentException))]
+        public void Extractor_ItShouldThrowAnExceptionWhenADatFileCannotBeFound()
+        {
+            String lstEntry = "aircraft/aircraft.dnm";
+
+            Ysfo.Core.Loader.DatLoader.GetDatFileFromLstEntry(lstEntry);
+        }
+    }
+}
