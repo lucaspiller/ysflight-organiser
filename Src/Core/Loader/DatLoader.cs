1bfb75575c454ded2ee8ff8b5812c4b12d5df27f
Luca Spiller <luca@stackednotion.com>
2009-08-17 22:09:38 +0100

    Implement main body of DatLoader

--- Src/Core/Loader/DatLoader.cs
+++ Src/Core/Loader/DatLoader.cs
@@ -2,11 +2,56 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.IO;
 
 namespace Ysfo.Core.Loader
 {
     public static class DatLoader
     {
+        public static void Load(String ysPath, String lstEntry, Dictionary<Regex, StringSetDelegate> regexes)
+        {
+            // get dat file
+            String datFile = GetDatFileFromLstEntry(lstEntry);
+
+            // check ysPath
+            if (ysPath == null)
+            {
+                throw new ArgumentException("ysPath is null.");
+            }
+
+            if (!Directory.Exists(ysPath))
+            {
+                throw new ArgumentException("Invalid ysPath; directory does not exist.");
+            }
+
+            // combine dat file with base dir to get full path
+            datFile = Path.GetFullPath(Path.Combine(ysPath, datFile));
+
+            // open file
+            using (var file = File.OpenText(datFile))
+            {
+                // read each line, ensuring not null (EOF)
+                String line;
+                while ((line = file.ReadLine()) != null)
+                {
+                    // for each regex
+                    regexes.Keys.ForEach(r =>
+                    {
+                        // check if matches
+                        Match m = r.Match(line);
+                        if (m.Success)
+                        {
+                            // if so call delegate with result
+                            regexes[r](m.Groups[1].Value);
+                        }
+                    });
+                }
+            }
+        }
+
+        public delegate void StringSetDelegate(String value);
+
         public static String GetDatFileFromLstEntry(String lstEntry)
         {
             // split lst
--- Tests/Core/Loader/DatLoaderFixture.cs
+++ Tests/Core/Loader/DatLoaderFixture.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using NUnit.Framework;
 
 namespace Ysfo.Tests.Core.Loader
@@ -9,6 +10,57 @@ namespace Ysfo.Tests.Core.Loader
     [TestFixture]
     class DatLoaderFixture
     {
+        String validYsPath;
+
+        [SetUp]
+        public void Setup()
+        {
+            validYsPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+        }
+
+        [Test]
+        public void ItShouldLoadTheAddonName()
+        {
+            String identify = null;
+
+            Dictionary<Regex, Ysfo.Core.Loader.DatLoader.StringSetDelegate> regexes = new Dictionary<Regex, Ysfo.Core.Loader.DatLoader.StringSetDelegate>() {
+                { new Regex("IDENTIFY (.*)"), delegate(String value) { identify = value; } }
+            };
+
+            Ysfo.Core.Loader.DatLoader.Load(validYsPath, "test.dat", regexes);
+
+            Assert.AreEqual("TEST_ADDON", identify);
+        }
+
+        [Test]
+        public void ItShouldLoadOtherAddonParameters()
+        {
+            String identify = null;
+            String test = null;
+
+            Dictionary<Regex, Ysfo.Core.Loader.DatLoader.StringSetDelegate> regexes = new Dictionary<Regex, Ysfo.Core.Loader.DatLoader.StringSetDelegate>() {
+                { new Regex("IDENTIFY (.*)"), delegate(String value) { identify = value; } },
+                { new Regex("TEST (.*)"), delegate(String value) { test = value; } }
+            };
+
+            Ysfo.Core.Loader.DatLoader.Load(validYsPath, "test.dat", regexes);
+
+            Assert.AreEqual("TEST_ADDON", identify);
+            Assert.AreEqual("TRUE", test);
+        }
+
+        [Test, ExpectedException(typeof(ArgumentException))]
+        public void ItShouldThrowAnExceptionIfTheLstEntryIsInvalid()
+        {
+            Ysfo.Core.Loader.DatLoader.Load(validYsPath, "invalid", null);
+        }
+
+        [Test, ExpectedException(typeof(ArgumentException))]
+        public void ItShouldThrowAnExceptionIfTheBaseDirIsInvalid()
+        {
+            Ysfo.Core.Loader.DatLoader.Load(null, "test.dat", null);
+        }
+
         [Test]
         public void Extractor_ItShouldReturnDatFileFromLstEntry()
         {
