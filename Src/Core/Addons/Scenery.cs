65a334b8b90867da65fd10ac7a7a61cc6822d93d
Luca Spiller <luca@stackednotion.com>
2009-08-18 14:16:44 +0100

    Add Scenery addon

---
+++ Src/Core/Addons/Scenery.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ysfo.Core
+{
+    public class Scenery : Internal.Addon
+    {
+        public override void Load(String ysPath)
+        {
+            ParseLstEntry();
+        }
+
+        protected void ParseLstEntry()
+        {
+            // split lst
+            String[] split = LstEntry.Split(' ');
+
+            // name is first entry in lst file
+            if (split.Length > 1)
+            {
+                Name = split[0];
+            }
+        }
+    }
+}
---
+++ Tests/Core/Addons/SceneryFixture.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Ysfo.Tests.Core.Addons
+{
+    [TestFixture]
+    class SceneryFixture
+    {
+        String _validYsPath;
+
+        [SetUp]
+        public void Setup()
+        {
+            _validYsPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+        }
+
+        [Test]
+        public void LoadMustLoadName()
+        {
+            var addon = new Ysfo.Core.Scenery { LstEntry = "TEST_SCENERY test.srf" };
+            addon.Load(_validYsPath);
+
+            Assert.AreEqual("TEST_SCENERY", addon.Name);
+        }
+    }
+}
